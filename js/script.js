62808e7c5c1a05965c78b369c6ff232e927d494d
mariel <zivy.mma@gmail.com>
2026-04-08 13:22:08 -0600

    Graficas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-
-const buscador = document.getElementById("buscador");
+/*     BUSCADOR DE TARJETAS DE CLIENTES
+ */const buscador = document.getElementById("buscador");
 const tarjetas = document.querySelectorAll(".client-card");
 
 buscador.addEventListener("keyup", function () {
@@ -19,3 +19,50 @@ card.style.display = "none";
 });
 
 });
+
+
+/*     GRAFICA DE BARRAS DE LOS SERVICIOS
+
+ */
+let datos = {
+transporte:88,
+almacen:72,
+coordinacion:95,
+tecnologia:60,
+cliente:80
+}
+
+function colorPorcentaje(valor){
+
+if(valor >= 85){
+return "#28a745"
+}
+
+else if(valor >= 70){
+return "#d98a1e"
+}
+
+else{
+return "#d9381e"
+}
+
+}
+
+function animar(){
+
+for(let area in datos){
+
+let barra = document.getElementById(area)
+let valor = datos[area]
+
+barra.style.height = valor + "%"
+barra.style.background = colorPorcentaje(valor)
+
+barra.querySelector(".porcentaje").innerText = valor + "%"
+
+}
+
+}
+
+window.onload = animar
+
